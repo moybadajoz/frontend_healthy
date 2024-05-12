d498c1075c6b49fe54232d75ca160d6131585539
moybadajoz <moisesbadajoz36@gmail.com>
2024-05-11 19:17:20 -0600

    navigation-drawer arreglado, base para dashboard y schedule empezado

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,5 +1,22 @@
 <template>
-  <div />
+  <div>
+    <v-row>
+      <v-text-field
+        outlined
+        append-icon="mdi-magnify"
+        dense
+        hide-details
+      />
+    </v-row>
+    <v-row>
+      <v-col cols="4">
+        <h4>Next appointments</h4>
+        <v-card>
+          dad
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
