181b7804c3d8216a3236494d383fc548c1b5782a
moybadajoz <moisesbadajoz36@gmail.com>
2024-05-28 16:24:16 -0600

    orders listo

--- pages/orders/index.vue
+++ pages/orders/index.vue
@@ -31,14 +31,18 @@
     </v-row>
     <v-container
       class="mt-6"
+      fluid
+      style="display: flex; flex-wrap: wrap; justify-content: space-around;"
     >
       <div
-        style="width: 25%;"
+        v-for="medicina in medicinas"
+        :key="medicina.nombre"
+        style="width: 30%; height: 40vh;"
       >
         <v-card
           rounded
-          color="#F8000033"
-          style="border-radius: 20px!important;"
+          color="#FEB2A0"
+          style="border-radius: 20px!important; height: 30vh;"
         >
           <v-card-title style="justify-content: center;">
             <v-avatar
@@ -46,17 +50,17 @@
               width="11vh"
               height="11vh"
             >
-              <v-img :src="require('@/assets/images/orders/metformin.svg')" />
+              <v-img :src="require(`@/assets/images/medicinas/${medicina.imagen}.jpg`)" />
             </v-avatar>
           </v-card-title>
           <v-card-subtitle style="display: grid;" class="mt-0">
             <h3 style="justify-self: center;">
-              Metformin
+              {{ medicina.nombre }}
             </h3>
           </v-card-subtitle>
           <v-card-text>
             <h3>
-              the first medicine prescribed for type 2 diabetes. It works mainly by lowering glucose production in the liver and improving the body's sensitivity to insulin so it uses insulin more effectively
+              {{ medicina.descripcion }}
             </h3>
           </v-card-text>
         </v-card>
@@ -79,6 +83,21 @@
 
 export default {
   layout: 'dashboard',
-  auth: true
+  auth: true,
+  data () {
+    return {
+      medicinas: [
+        { nombre: 'Ibuprofeno', descripcion: 'Antiinflamatorio no esteroideo (AINE) utilizado para aliviar el dolor, la fiebre y la inflamación.', imagen: 'ibuprofeno' },
+        { nombre: 'Paracetamol (Acetaminofén)', descripcion: 'Analgésico y antipirético utilizado para tratar el dolor y reducir la fiebre.', imagen: 'paracetamol' },
+        { nombre: 'Amoxicilina', descripcion: 'Antibiótico de amplio espectro usado para tratar diversas infecciones bacterianas.', imagen: 'amoxicilina' },
+        { nombre: 'Metformina', descripcion: 'Medicamento antidiabético que ayuda a controlar los niveles de azúcar en sangre en personas con diabetes tipo 2.', imagen: 'metformina' },
+        { nombre: 'Loratadina', descripcion: 'Antihistamínico utilizado para aliviar los síntomas de alergias como estornudos, picazón y secreción nasal.', imagen: 'loratadina' },
+        { nombre: 'Omeprazol', descripcion: 'Inhibidor de la bomba de protones que reduce la producción de ácido estomacal, utilizado para tratar el reflujo gastroesofágico y úlceras.', imagen: 'omeprazol' },
+        { nombre: 'Atorvastatina', descripcion: 'Estatina que se usa para reducir los niveles de colesterol y prevenir enfermedades cardiovasculares.', imagen: 'atorvastatina' },
+        { nombre: 'Aspirina (Ácido acetilsalicílico)', descripcion: 'AINE usado para aliviar el dolor, reducir la fiebre y actuar como anticoagulante para prevenir ataques cardíacos.', imagen: 'aspirina' },
+        { nombre: 'Alprazolam', descripcion: 'Benzodiazepina utilizada para tratar trastornos de ansiedad y pánico.', imagen: 'alprazolam' }
+      ]
+    }
+  }
 }
 </script>
